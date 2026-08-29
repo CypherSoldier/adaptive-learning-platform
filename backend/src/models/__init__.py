58add6e12f772ec6b3d7b1c158f3d123c985d317
Caleb Wagner <calebwagner200@gmail.com>
2026-08-29 18:58:21 +0200

    imported models for SQLAlchemy seeding

---
+++ backend/src/models/__init__.py
@@ -0,0 +1,15 @@
+from src.models.user import User
+from src.models.learning_track import LearningTrack
+from src.models.question import Question
+from src.models.topic import Topic
+from src.models.submission import Submission
+from src.models.user_skill_profile import UserSkillProfile
+
+__all__ = [
+    "User",
+    "LearningTrack",
+    "Question",
+    "Topic",
+    "Submission",
+    "UserSkillProfile",
+]
